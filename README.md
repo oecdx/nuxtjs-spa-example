1636681c23087ad7fb2756f5d119f1e5d728adb1
ymatsuo <ymatsuo@oec.co.jp>
2020-05-13 15:58:31 +0900

    Nuxt.js実装例
    実装例とREADMEの更新

--- README.md
+++ README.md
@@ -1,22 +1,96 @@
 # nuxtjs-spa-example
 
-> My awesome Nuxt.js project
+> Nuxt.js の実装例
+
+## 目次
+
+- [Build Setup](#Build-setup)
+- [ルートディレクトリのファイル・フォルダ説明](#ルートディレクトリのファイル・フォルダ説明)
+- [関連リンク](#関連リンク)
+- [その他](#その他)
 
 ## Build Setup
 
 ```bash
-# install dependencies
+# 依存パッケージの追加
 $ yarn install
 
-# serve with hot reload at localhost:3000
+# localhost:3000で開発用Webサーバを起動（ホットリロードに対応）
 $ yarn dev
 
-# build for production and launch server
+# productionビルドとその実行
 $ yarn build
 $ yarn start
 
-# generate static project
+# 静的projectの作成
 $ yarn generate
 ```
+
+## ルートディレクトリのファイル・フォルダ説明
+
+- assets
+
+  - css/sass など、webpack でコンパイルしたいファイルを置くディレクトリ
+  - js ファイルは/plugin に置くことが多いので、主にスタイル関連や web フォントなどを置くことになる
+  - ファイルパスは「~/assets/」から指定する
+
+- components
+
+  - 共通部品となる vue ファイルを置くディレクトリ
+  - 単体でページを作るのではなく、layouts や pages 配下の vue ファイルから import されて部品として呼び出される
+
+- layouts
+
+  - アプリケーション全体の共通フレーム（メニューとメイン部分の骨組みなど）を定義したファイルを置くディレクトリ
+
+- middleware
+
+  - ページが描画される前に実行したい共通処理（ログインチェックなど）を書いた js を置くディレクトリ
+
+- pages
+
+  - メインとなる画面を置くディレクトリ
+  - デフォルトではここの vue ファイル名が URL として使われる（アドレスバーに表示される）
+
+- plugins
+
+  - js の共通部品などを置くディレクトリ
+
+- static
+
+  - 画像などの静的ファイルを置くディレクトリ
+  - webpack でコンパイルしないファイルを置く
+  - assets と異なり、アクセスするときのパスは「~/static/」ではなく static をルートとして「/」から始める
+
+- store
+
+  - 複数ページを跨ぐデータを扱う変数を定義する
+  - ここで定義した変数の値はページ遷移しても初期化されないので初期化忘れに注意
+  - spa なので通常はリロード(F5)すると消えてしまう（回避するには [vuex-persistedstate](#関連リンク) などを使う）
+
+- nuxt.config.js
+
+  - nuxt の設定ファイル
+  - middleware や plugin の読み込み、ビルド時の設定など諸々ここで指定する
+
+- .env
+
+  - [@nuxtjs/dotenv](#関連リンク)が使用する設定ファイル
+  - 環境変数の代わりにこのファイルで認証情報などを定義する
+
+    書き方
+
+    ```Text
+    key=value
+    ```
+
+（注）node.js が標準で使用するファイル（packaga.json など）はここでは扱いません
+
+## 関連リンク
+
+- [vuex-persistedstate](https://github.com/robinvdvleuten/vuex-persistedstate)
+- [@nuxtjs/dotenv](https://github.com/nuxt-community/dotenv-module)
+
+## その他
 
 For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,160 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        nuxtjs-spa-example
-      </h1>
-      <h2 class="subtitle">
-        My awesome Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+  <v-container fluid>
+    <v-row class="pt-12">
+      <v-col col6>
+        <div class="file-upload">
+          <div class="image-upload-wrap" v-if="imageSrc === null">
+            <input
+              @change="readURL($event)"
+              type="file"
+              name="file"
+              class="file-upload-input"
+              accept="image/*"
+              ref="uploadInput"
+            />
+            <div class="drag-text">
+              <h3>Drag and drop a file or select add Image</h3>
+            </div>
+          </div>
+          <div class="file-upload-content" v-if="imageSrc != null">
+            <img class="file-upload-image" :src="imageSrc" alt="your image" />
+            <img
+              class="file-upload-image"
+              v-if="responseImgSrc != null"
+              :src="responseImgSrc"
+              alt="response image"
+            />
+          </div>
+          <v-btn class="mt-3" block @click="uploadClick($event)" v-if="imageSrc === null">写真を登録</v-btn>
+          <v-btn class="mt-3" block @click="removeUpload" v-if="imageSrc != null">削除</v-btn>
+          <v-card class="mt-3" v-for="(result,index) in resultJson" v-bind:key="index">
+            <v-card-text v-for="(emotion, subIndex) in result.Emotions" v-bind:key="subIndex">
+              <div>感情：{{ emotion.Type | japanize }}</div>
+              <div>確信度：{{ emotion.Confidence }}</div>
+            </v-card-text>
+          </v-card>
+          <v-card class="mt-3" v-if="resultJson">
+            <v-card-text>
+              <list-inner v-if="resultJson" :results="resultJson" :label="''" />
+            </v-card-text>
+          </v-card>
+          <v-card class="mt-3" v-if="resultJson">
+            <v-card-title>元データ</v-card-title>
+            <v-card-text style="white-space:pre-wrap; word-wrap:break-word;">
+              {{
+              resultJsonStr
+              }}
+            </v-card-text>
+          </v-card>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import ListInner from '~/components/ListInner'
 
 export default {
+  data() {
+    return {
+      title: 'rekognition-test',
+      imageSrc: null,
+      responseImgSrc: null,
+      resultJson: ''
+    }
+  },
   components: {
-    Logo
+    ListInner
+  },
+  filters: {
+    japanize: function(value) {
+      var emotionMapping = {
+        FEAR: '恐怖',
+        HAPPY: '幸福',
+        ANGRY: '怒り',
+        SAD: '悲しみ',
+        DISGUSTED: '嫌悪',
+        CALM: '穏やか',
+        CONFUSED: '混乱',
+        SURPRISED: '驚き'
+      }
+
+      if (emotionMapping[value]) {
+        return value + '（' + emotionMapping[value] + '）'
+      } else {
+        return value ? value : ''
+      }
+    }
+  },
+  computed: {
+    resultJsonStr() {
+      if (this.resultJson) {
+        return JSON.stringify(this.resultJson, null, 4)
+      } else {
+        return this.resultJson
+      }
+    }
+  },
+  head() {
+    return {
+      bodyAttrs: {},
+      script: [],
+      link: []
+    }
+  },
+  mounted() {
+    // 子コンポーネントのロードを無視して実行
+    this.$nextTick(() => {})
+  },
+  destroyed: function() {},
+  methods: {
+    uploadClick($event) {
+      const elem = this.$refs.uploadInput
+      elem.click()
+    },
+    readURL(e) {
+      let input = e.target
+      if (input.files && input.files[0]) {
+        let fr = new FileReader()
+        fr.onloadend = event => {
+          let base64Content = fr.result
+          this.imageSrc = base64Content
+          // TODO あとでKeyは外部化
+          let requestBody = {
+            image: base64Content.replace('data:image/jpeg;base64,', '')
+          }
+
+          let config = {
+            headers: {
+              'content-type': 'application/json'
+            }
+          }
+          this.$axios
+            // .post('/api/nuxtPost', requestBody, config)
+            .post(process.env.AWS_API_URL, requestBody, config)
+            .then(response => {
+              console.log(response.data.results)
+              if (
+                response.data.results.FaceDetails &&
+                response.data.results.FaceDetails.length > 0
+              ) {
+                this.resultJson = response.data.results.FaceDetails
+              }
+            })
+            .catch(error => {
+              // error 処理
+              console.log(JSON.stringify(error))
+            })
+        }
+        fr.readAsDataURL(input.files[0])
+      }
+    },
+    removeUpload() {
+      this.imageSrc = null
+      this.responseImgSrc = null
+      this.resultJson = ''
+    }
   }
 }
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
